6b66625d75a2acf5a74a6ea57d160d2561bf3536
Riel St. Amand <riel.stamand@gmail.com>
2026-09-17 15:53:33 -0400

    Clean up X card rendering: counts, logo, clamp

--- test/system/x_post_cards_test.rb
+++ test/system/x_post_cards_test.rb
@@ -40,12 +40,29 @@ class XPostCardsTest < ApplicationSystemTestCase
     assert_selector ".x-post-card__link", text: "View on X"
   end
 
+  test "long post text stays clamped until Show more is used" do
+    stub_fxtwitter_post(status: "141", text: "Lorem ipsum dolor sit amet. " * 40)
+
+    sign_in "jz@37signals.com"
+    join_room rooms(:designers)
+
+    send_message "check https://x.com/jack/status/141 out"
+    perform_enqueued_jobs
+
+    assert_selector ".x-post-card__clamp", text: "Lorem ipsum"
+    assert_selector ".x-post-card__details .x-post-card__text", visible: :hidden
+
+    click_on "Show more"
+
+    assert_selector ".x-post-card__details[open] .x-post-card__text", text: "Lorem ipsum"
+  end
+
   private
-    def stub_fxtwitter_post
+    def stub_fxtwitter_post(status: "140", text: "just setting up my twttr")
       tweet = {
-        "url" => "https://x.com/jack/status/140",
-        "id" => "140",
-        "text" => "just setting up my twttr",
+        "url" => "https://x.com/jack/status/#{status}",
+        "id" => status,
+        "text" => text,
         "created_at" => "Tue Mar 21 20:50:14 +0000 2006",
         "created_timestamp" => 1142974214,
         "likes" => 310826,
@@ -58,7 +75,7 @@ class XPostCardsTest < ApplicationSystemTestCase
         }
       }
 
-      stub_request(:get, "https://api.fxtwitter.com/jack/status/140")
+      stub_request(:get, "https://api.fxtwitter.com/jack/status/#{status}")
         .to_return(status: 200, body: { code: 200, message: "OK", tweet: tweet }.to_json,
           headers: { "Content-Type" => "application/json" })
     end
